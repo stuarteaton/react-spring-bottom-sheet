c8cb3c3719f3b78764ba21a3614cff2b7a6eea70
megaarmos <paularion007@gmail.com>
2024-12-06 18:01:25 -0500

    complete rewrite;

--- src/App.vue
+++ src/App.vue
@@ -1,218 +1,241 @@
 <template>
   <button @click="open()">Open</button>
   <div class="sheet-container">
-    <transition><div class="overlay" ref="ovelay" @click="overlayClick()" v-show="showOverlay"></div></transition>
+    <Transition name="fade">
+      <div class="sheet-overlay" ref="overlay" @click="overlayClick()" v-show="showOverlay"/>
+    </Transition>
     <div class="sheet" ref="sheet">
-      <div class="sheet-handle" ref="drag"></div>
-      <div class="sheet-content">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime neque sed tempore ullam.
-          Accusamus animi at autem beatae est, hic inventore iste minima, non quibusdam quisquam
-          quos velit veritatis voluptatem?
-        </p>
+      <div class="sheet-handle" ref="sheetHandle"/>
+
+      <div class="sheet-content" ref="sheetContent">
+        <div>
+          <button>test</button>
+        </div>
+        <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+          award-winning fireworks and seasonal special events.</p>
+        <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+          award-winning fireworks and seasonal special events.</p>
+        <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+          award-winning fireworks and seasonal special events.</p>
+        <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+          award-winning fireworks and seasonal special events.</p>
+        <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+          award-winning fireworks and seasonal special events.</p>
+        <details>
+          <summary>Epcot Center</summary>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+          <p>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions,
+            award-winning fireworks and seasonal special events.</p>
+        </details>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {ref, computed} from 'vue'
-import {useDrag} from '@vueuse/gesture'
-import {useSpring, useMotionProperties} from '@vueuse/motion'
+import {computed, onMounted, ref, watch} from 'vue'
+import {useElementSize, useWindowSize} from '@vueuse/core'
+import {useDrag, useGesture} from '@vueuse/gesture'
+import {useElementTransform} from '@vueuse/motion'
 
-import {useWindowSize} from '@vueuse/core'
+const sheet = ref<HTMLElement | null>(null);
+const sheetHandle = ref<HTMLElement | null>(null);
+const sheetContent = ref<HTMLElement | null>(null);
+const isSheetContentScrolling = ref(false)
 
-const {height} = useWindowSize()
-const heightString = computed(() => {
-  return `${height.value}px`
-})
+const overlay = ref<HTMLElement | null>(null);
+const showOverlay = ref<boolean>(false);
 
-const drag = ref(null)
-const sheet = ref(null)
+const {height: sheetHeight} = useElementSize(sheet)
 
-const translationY = ref(0)
-const maxHeight = ref(640)
-const maxHeightString = computed(() => {
-  return `${maxHeight.value}px`
-})
+const {transform} = useElementTransform(sheet)
 
-const showOverlay = ref(false);
-const overlayDismiss = ref(true);
-const swipeToDismiss = ref(true)
-
-const breakpoints = ref<number[]>([0.25, .5, 1])
+const initialBreakpoint = 0.5;
+const currentBreakpointIndex = ref(0);
+const breakpoints = new Set([0.25, 0.5, 1]);
 const sortedBreakpoints = computed(() => {
-  if (swipeToDismiss) {
-    return [0, ...breakpoints.value].sort((a: number, b: number) => a - b)
-  }
-  return [...breakpoints.value].sort((a: number, b: number) => a - b)
-})
+  return [...breakpoints].sort((a, b) => a - b)
+});
 
-// Get motions
-const {motionProperties} = useMotionProperties(sheet, {
-  y: 0
-})
+const translateY = ref(0)
+const closestBreakpoint = ref(0)
 
-const {set, stop} = useSpring(motionProperties, {
-  damping: 14,
-  stiffness: 75,
-  mass: .75,
-})
+const canSwiperClose = ref(true)
+const canOverlayClose = ref(true)
+const canDragContent = ref(true)
 
-const open = () => {
-  // document.documentElement.style.overflowY = 'hidden'
-  // document.documentElement.style.overscrollBehavior = 'none'
-  set({
-    y: -300,
-  })
-  translationY.value = -300
-  showOverlay.value = true
-}
-
-const close = () => {
-  // document.documentElement.style.overflowY = 'auto'
-  // document.documentElement.style.overscrollBehavior = ''
-  set({
-    y: 0,
-  })
-  translationY.value = 0
-  showOverlay.value = false
-}
-
-const overlayClick = () => {
-  if (overlayDismiss.value) {
+const handleEscapeKey = (e: KeyboardEvent) => {
+  if (e.key === 'Escape') {
     close()
   }
 }
 
-useDrag(state => handler(state), {
-  domTarget: drag,
-})
+const open = () => {
+  if (!sheet.value) return
 
-// Drag handler
-const handler = (event) => {
-  // Check element existence
-  if (!drag) return
+  sheet.value.style.transition = 'all 0.3s ease-in-out'
+  translateY.value = (initialBreakpoint * sheetHeight.value - sheetHeight.value) * -1;
+  transform.translateY = translateY.value
+  showOverlay.value = true;
 
-  translationY.value += event.delta[1];
+  window.addEventListener('keydown', handleEscapeKey)
+}
 
-  if (translationY.value < sortedBreakpoints.value[sortedBreakpoints.value.length - 1] * maxHeight.value * -1) {
-    translationY.value = sortedBreakpoints.value[sortedBreakpoints.value.length - 1] * maxHeight.value * -1;
-  }
+const close = () => {
+  if (!sheet.value) return
 
-  // Apply movement values
-  set({
-    y: translationY.value,
-  })
+  sheet.value.style.transition = 'all 0.3s ease-in-out'
+  translateY.value = sheetHeight.value;
+  transform.translateY = translateY.value
+  showOverlay.value = false;
 
-  let closestBreakpoint;
-  // Reset the box at initial position
-  if (!event.dragging) {
-    const breakpointToSheet = sortedBreakpoints.value.map(breakpoint => breakpoint * maxHeight.value * -1)
-
-    closestBreakpoint = breakpointToSheet.reduce((prev, curr) => {
-      return Math.abs(curr - translationY.value) < Math.abs(prev - translationY.value) ? curr : prev;
-    });
-
-
-    translationY.value = closestBreakpoint;
-    set({
-      y: closestBreakpoint,
-    })
-
-    if (closestBreakpoint === 0) {
-      close()
-    }
+  window.removeEventListener('keydown', handleEscapeKey)
+}
+const overlayClick = () => {
+  if (canOverlayClose.value) {
+    close()
   }
 }
+
+function snapToBreakpoint() {
+  const breakpointToSheet = sortedBreakpoints.value.map(breakpoint => (breakpoint * sheetHeight.value - sheetHeight.value) * -1)
+
+  closestBreakpoint.value = breakpointToSheet.reduce((prev, curr) => {
+    return Math.abs(curr - translateY.value) < Math.abs(prev - translateY.value) ? curr : prev;
+  });
+
+  sheet.value!.style.transition = 'all 0.3s ease-in-out'
+
+  translateY.value = closestBreakpoint.value
+
+  if (canSwiperClose.value && translateY.value === sheetHeight.value) {
+    close();
+  }
+}
+
+useGesture(
+    {
+      onDrag: ({delta}) => {
+        if (!sheet.value) return
+
+        translateY.value += delta[1];
+        if (translateY.value < 0) {
+          translateY.value = 0
+        }
+        if (translateY.value < (sortedBreakpoints.value[sortedBreakpoints.value.length - 1] * sheetHeight.value - sheetHeight.value) * -1) {
+          translateY.value = (sortedBreakpoints.value[sortedBreakpoints.value.length - 1] * sheetHeight.value - sheetHeight.value) * -1
+        }
+
+        sheet.value.style.transition = ''
+        transform.translateY = translateY.value
+      },
+      onDragEnd: () => {
+        snapToBreakpoint();
+        transform.translateY = translateY.value
+      },
+    },
+    {
+      domTarget: sheetHandle,
+    }
+)
+
+if (canDragContent.value) {
+  useGesture(
+      {
+        onDrag: ({delta}) => {
+          if (!sheet.value) return
+
+          translateY.value += delta[1];
+          if (translateY.value < 0) {
+            translateY.value = 0
+          }
+          if (translateY.value < (sortedBreakpoints.value[sortedBreakpoints.value.length - 1] * sheetHeight.value - sheetHeight.value) * -1) {
+            translateY.value = (sortedBreakpoints.value[sortedBreakpoints.value.length - 1] * sheetHeight.value - sheetHeight.value) * -1
+          }
+
+          sheet.value.style.transition = ''
+          transform.translateY = translateY.value
+        },
+        onDragEnd: () => {
+          snapToBreakpoint();
+          transform.translateY = translateY.value
+        },
+      },
+      {
+        domTarget: sheetContent,
+        drag: {
+          filterTaps: true,
+          delay: 300,
+        }
+      }
+  )
+}
+
+watch(
+    sheetHeight,
+    async () => {
+      snapToBreakpoint()
+    }
+)
+
+onMounted(() => {
+  transform.translateY = sheetHeight.value;
+})
 </script>
 
 <style>
-* {
-  box-sizing: border-box;
-}
-
 body {
+  width: 100%;
   min-height: 100dvh;
 }
 
 #app {
+  padding: 0;
   width: 100%;
-  overflow: hidden;
-}
-
-.overlay {
-  position: absolute;
-  inset: 0;
-  background: rgba(0, 0, 0, 0.5);
-  z-index: -1;
-  pointer-events: auto;
 }
 
 .sheet-container {
-  isolation: isolate;
+  z-index: 99999;
+  max-height: 100dvh;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  overflow: hidden;
   position: absolute;
   inset: 0;
-  overflow: hidden;
   pointer-events: none;
+  isolation: isolate;
+}
+
+.sheet {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  width: 640px;
+  height: fit-content;
+  max-height: inherit;
+  border-radius: 16px 16px 0 0;
+  background-color: #535bf2;
+  bottom: 0;
+  pointer-events: all;
 }
 
 .sheet-handle {
@@ -221,28 +244,26 @@ body {
   background: red;
 }
 
-.sheet {
-  display: flex;
-  flex-direction: column;
-  pointer-events: auto;
+.sheet-overlay {
+  z-index: -1;
   position: absolute;
-  top: 100%;
-  width: 100%;
-  max-width: 640px;
-  height: v-bind(heightString);
-  max-height: v-bind(maxHeightString);
-  background-color: rgba(139, 92, 246, 0.8);
-  border-radius: 16px 16px 0 0;
-  overflow: hidden;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  pointer-events: auto;
+  user-select: none;
 }
 
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 300ms ease;
+.sheet-content {
+  overflow-y: auto;
 }
 
-.v-enter-from,
-.v-leave-to {
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
 }
 </style>
