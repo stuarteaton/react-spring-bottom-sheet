5a670fbac898f6c5d3c84e7fdd24bc1108122d1d
megaarmos <paularion007@gmail.com>
2024-12-08 01:51:56 -0500

    feat: expandOnContentDrag option; feat: sticky header and footer;

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import {ref, useTemplateRef } from 'vue'
 import BottomSheet from "./components/BottomSheet.vue";
+import type { ComponentExposed } from 'vue-component-type-helpers'
 
-const myBottomSheet = ref(null)
+const myBottomSheet = useTemplateRef<ComponentExposed<typeof BottomSheet>>('myBottomSheet')
 const maxHeight = ref(0)
 const minHeight = ref(0)
 
@@ -10,6 +11,9 @@ const open = () => {
   myBottomSheet.value?.open();
 }
 
+const close = () => {
+  myBottomSheet.value?.close();
+}
 </script>
 
 <template>
@@ -20,10 +24,27 @@ const open = () => {
       v-model:min-height="minHeight"
       :snap-points="[maxHeight / 3, maxHeight / 1.5]"
       :can-overlay-close="true"
-      :can-swipe-close="true">
+      :can-swipe-close="true"
+      :expandOnContentDrag="true"
+  >
+    <template #header>
+      <h1 style="font-size: 24px; margin: 0; text-align: center">Header</h1>
+    </template>
     <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
     <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
     <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <p>lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur sagittis, nunc</p>
+    <template #footer>
+      <button type="button" @click="close">Close bottom sheet</button>
+    </template>
   </BottomSheet>
 </template>
 
--- src/components/BottomSheet.vue
+++ src/components/BottomSheet.vue
@@ -9,27 +9,29 @@ interface IProps {
   defaultBreakpoint?: number
   canSwipeClose?: boolean
   canOverlayClose?: boolean
+  expandOnContentDrag?: boolean
 }
 
 const props = withDefaults(defineProps<IProps>(), {
   canSwipeClose: true,
-  canOverlayClose: true
+  canOverlayClose: true,
+  expandOnContentDrag: true
 })
 
 const maxHeight = defineModel('maxHeight')
 const minHeight = defineModel('minHeight')
 
 const sheet = ref<HTMLElement | null>(null);
-const sheetHandle = ref<HTMLElement | null>(null);
+const sheetHeader = ref<HTMLElement | null>(null);
 const sheetContentWrapper = ref<HTMLElement | null>(null);
 
 const overlay = ref<HTMLElement | null>(null);
-const showOverlay = ref<boolean>(false);
+const showSheet = ref<boolean>(false);
 
 const {height: windowHeight} = useWindowSize()
 const {height: sheetHeight} = useElementSize(sheet)
 const {height: sheetContentWrapperHeight} = useElementSize(sheetContentWrapper)
-const {height: sheetHandleHeight} = useElementSize(sheetHandle)
+const {height: sheetHandleHeight} = useElementSize(sheetHeader)
 const minHeightComputed = computed(() => {
   return sheetContentWrapperHeight.value + sheetHandleHeight.value
 })
@@ -58,7 +60,7 @@ const open = () => {
   height.value = props.defaultBreakpoint ? props.defaultBreakpoint : sortedBreakpoints.value[0];
   style.height = props.defaultBreakpoint ? props.defaultBreakpoint : sortedBreakpoints.value[0];
   transform.translateY = 0;
-  showOverlay.value = true;
+  showSheet.value = true;
 
   window.addEventListener('keydown', handleEscapeKey)
 }
@@ -68,7 +70,7 @@ const close = () => {
 
   sheet.value.style.transition = 'all 0.3s ease-in-out'
   transform.translateY = sheetHeight.value;
-  showOverlay.value = false;
+  showSheet.value = false;
 
   window.removeEventListener('keydown', handleEscapeKey)
 }
@@ -112,7 +114,7 @@ useGesture(
           if (props.canSwipeClose) {
             transform.translateY = translateY.value;
           } else {
-            transform.translateY = rubberbandIfOutOfBounds(translateY.value, - sheetHeight.value, 0, 0.5);
+            transform.translateY = rubberbandIfOutOfBounds(translateY.value, -sheetHeight.value, 0, 0.5);
           }
         }
 
@@ -123,7 +125,6 @@ useGesture(
         if (!sheet.value) return;
 
         findClosestIndexBreakpoint();
-
 
         translateY.value = props.canSwipeClose
             ? [0, height.value].reduce((prev, curr) => {
@@ -145,14 +146,82 @@ useGesture(
       },
     },
     {
-      domTarget: sheetHandle,
+      domTarget: sheetHeader,
       drag: {
-        preventWindowScrollY: true,
+        // preventWindowScrollY: true,
         filterTaps: false,
       },
     }
 );
 
+
+if (props.expandOnContentDrag) {
+  useGesture(
+      {
+        onDrag: ({delta}) => {
+          if (!sheet.value) return;
+
+
+          if (translateY.value === 0) {
+            height.value -= delta[1];
+          }
+
+          if (height.value <= sortedBreakpoints.value[0]) {
+            height.value = sortedBreakpoints.value[0];
+
+            translateY.value += delta[1];
+
+            if (translateY.value >= sortedBreakpoints.value[0]) {
+              translateY.value = sortedBreakpoints.value[0];
+            }
+            if (translateY.value <= 0) {
+              translateY.value = 0;
+            }
+
+            if (props.canSwipeClose) {
+              transform.translateY = translateY.value;
+            } else {
+              transform.translateY = rubberbandIfOutOfBounds(translateY.value, -sheetHeight.value, 0, 0.5);
+            }
+          }
+
+          sheet.value.style.transition = '';
+          style.height = rubberbandIfOutOfBounds(height.value, 0, sortedBreakpoints.value[sortedBreakpoints.value.length - 1], 0.25);
+        },
+        onDragEnd: () => {
+          if (!sheet.value) return;
+
+          findClosestIndexBreakpoint();
+
+
+          translateY.value = props.canSwipeClose
+              ? [0, height.value].reduce((prev, curr) => {
+                return Math.abs(curr - translateY.value) < Math.abs(prev - translateY.value)
+                    ? curr
+                    : prev
+              })
+              : 0;
+          transform.translateY = translateY.value;
+
+          if (translateY.value === height.value) {
+            translateY.value = 0;
+            close();
+          }
+
+          sheet.value.style.transition = 'all 0.3s ease-in-out';
+          height.value = sortedBreakpoints.value[currentBreakpointIndex.value];
+          style.height = sortedBreakpoints.value[currentBreakpointIndex.value];
+        },
+      },
+      {
+        domTarget: sheetContentWrapper,
+        drag: {
+          // preventWindowScrollY: true,
+          filterTaps: false,
+        },
+      }
+  );
+}
 
 onMounted(() => {
   maxHeight.value = windowHeight.value;
@@ -171,10 +240,12 @@ defineExpose({open, close})
   <Teleport to="body">
     <div class="sheet-container">
       <Transition name="fade">
-        <div class="sheet-overlay" ref="overlay" @click="overlayClick()" v-show="showOverlay"/>
+        <div class="sheet-overlay" ref="overlay" @click="overlayClick()" v-show="showSheet"/>
       </Transition>
-      <div class="sheet" ref="sheet">
-        <div class="sheet-handle" ref="sheetHandle"/>
+      <div class="sheet" :class="showSheet && 'sheet-show'" ref="sheet">
+        <div class="sheet-header" ref="sheetHeader">
+          <slot name="header"></slot>
+        </div>
 
         <div class="sheet-scroll">
           <div ref="sheetContentWrapper">
@@ -183,12 +254,27 @@ defineExpose({open, close})
             </div>
           </div>
         </div>
+
+        <div class="sheet-footer">
+          <slot name="footer"></slot>
+        </div>
       </div>
     </div>
   </Teleport>
 </template>
 
 <style scoped>
+.sheet-overlay {
+  z-index: -1;
+  position: absolute;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  pointer-events: auto;
+  user-select: none;
+
+  will-change: opacity;
+}
+
 .sheet-container {
   z-index: 99999;
   max-height: 100dvh;
@@ -211,24 +297,50 @@ defineExpose({open, close})
   height: fit-content;
   max-height: inherit;
   border-radius: 16px 16px 0 0;
-  background-color: #535bf2;
+  background-color: #ffffff;
   bottom: 0;
   pointer-events: all;
+  visibility: hidden;
+
+  will-change: height;
 }
 
-.sheet-handle {
-  height: 36px;
-  flex-shrink: 0;
-  background: red;
+.sheet-show {
+  visibility: visible;
 }
 
-.sheet-overlay {
-  z-index: -1;
-  position: absolute;
-  inset: 0;
-  background-color: rgba(0, 0, 0, 0.5);
-  pointer-events: auto;
+.sheet-header {
   user-select: none;
+  padding: 20px 16px 8px 16px;
+  flex-shrink: 0;
+  box-shadow: 0 1px 0 rgba(46, 59, 66, calc(1 * .125));
+}
+
+.sheet-header:empty {
+  padding: 12px 16px 8px 16px;
+  box-shadow: none;
+}
+
+.sheet-header:before {
+  position: absolute;
+  content: "";
+  display: block;
+  width: 36px;
+  height: 4px;
+  top: 8px;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 2px;
+  background-color: rgba(0, 0, 0, .28);
+}
+
+.sheet-footer {
+  padding: 16px;
+  box-shadow: 0 -1px 0 rgba(46, 59, 66, 0.125), 0 2px 0 #fff;
+}
+
+.sheet-footer:empty {
+  display: none;
 }
 
 .sheet-scroll {
@@ -237,7 +349,18 @@ defineExpose({open, close})
 
 .sheet-content {
   display: grid;
+  padding: 1vh 3vh 3vh;
   user-select: none;
+}
+
+@media (prefers-color-scheme: dark) {
+  .sheet {
+    background-color: #242424;
+  }
+
+  .sheet-header:before {
+    background-color: rgba(255, 255, 255, 0.38);
+  }
 }
 
 .fade-enter-active,
