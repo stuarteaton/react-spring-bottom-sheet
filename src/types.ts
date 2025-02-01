49954ee7c8dc965568387ef42bf2bc45417bc632
N1cE-me <banicov2011@yandex.ru>
2025-02-01 18:12:08 +0500

    refactor(core): Moved the types to a separate file
    This fix vue-tsc errors on build with ref

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface BottomSheetProps {
+  duration?: number
+  snapPoints?: number[]
+  defaultSnapPoint?: number
+  blocking?: boolean
+  canSwipeClose?: boolean
+  canBackdropClose?: boolean
+  expandOnContentDrag?: boolean
+}
